da440afc9beff3a8a13546fc0e1114cceec786df
Shashwat Nautiyal <shashwatnautiyal2015@gmail.com>
2022-02-25 23:33:29 +0530

    viewport added in meta

--- app/root.tsx
+++ app/root.tsx
@@ -34,6 +34,8 @@ function Document({
 		<html lang="en">
 			<head>
 				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<Meta />
 				<title>{title}</title>
 				<Links />
