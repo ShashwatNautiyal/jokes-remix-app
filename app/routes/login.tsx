f73fcfebfdf9bf11315bc917b5a3b1e2278a0189
Shashwat Nautiyal <85606496+ShashwatNautiyal@users.noreply.github.com>
2022-11-23 03:17:27 +0530

    fix login label

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -245,7 +245,7 @@ export default function Login() {
 										type="submit"
 										className="w-full touch-manipulation flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 									>
-										actionData?.fields?.loginType === "login" ? Sign in : Register
+										{actionData?.fields?.loginType === "register" ? "Register" : "Sign in"}
 									</button>
 								</div>
 							</Form>
